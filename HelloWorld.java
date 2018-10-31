e357cbb22e555384bbe7a5d0b302365d69a1bc07
vmr159 <41227455+vmr159@users.noreply.github.com>
2018-10-31 21:20:19 +0530

    new156

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!!!!");
+		System.out.println("Hello world");
 
 	}
 
