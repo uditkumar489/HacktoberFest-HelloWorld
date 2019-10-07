dfd0e19dc784767b9ef375dd28369d8e60ddf7bf
Suchi Jha <55627605+suchi12j@users.noreply.github.com>
2019-10-08 00:25:47 +0530

    Hello Name

---
+++ Hello Name.cpp
@@ -0,0 +1,12 @@
+// 8 aug
+//WAP to print qoutes "Hello Name" where name is input from user
+#include<iostream>
+using namespace std;
+int main()
+{
+	string name;
+	cout<<"Enter NAME"<<endl;
+	cin>>name;
+	cout<<"Hello "<<name<<endl;
+	return 0;
+}
