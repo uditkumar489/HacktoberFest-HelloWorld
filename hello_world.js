f2b447dacf22ae18e2ab75c747e8cae14fc7664f
eprendes <prendese@my.westmoreland.edu>
2019-10-13 12:16:00 -0400

    Added Hello World
    HTML style

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+<!doctype html>
+<html>
+<head>
+<script>
+function hello()
+{
+alert("Hello World!");
+}
+</script>
+</head>
+<body>
+<button onclick="hello()">Click Me</button></br></br>
+</body>
+</html>
