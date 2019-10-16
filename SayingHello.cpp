c19b30d3a26f5c73eb45023ba757eff000683bda
Furkan E <56611490+aUniverseBeyond@users.noreply.github.com>
2019-10-17 00:20:11 +0300

    My "hello world"
    I've written "Hello World" pattern in C++

---
+++ SayingHello.cpp
@@ -0,0 +1,17 @@
+// this is my basic c++ knowledge which I will show the best "hello world" you've ever seen written in C++!
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+
+	cout << "Hello World!" << endl;
+
+
+	return 0;
+
+}
