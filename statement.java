e3508f5d3281cfbeb446717e97bc3a41bb373e89
Priyanshi <33222184+Priyanshisingh28@users.noreply.github.com>
2020-10-07 11:20:01 +0530

    java beginners program

---
+++ statement.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package program;
+
+/**
+ *
+ * @author HP
+ */
+public class statement
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("name=PRIYANSHI");
+        System.out.println("course=BTECH");
+        System.out.println("college=jecrc");
+        System.out.println("add=jaipur");// TODO code application logic here
+    }
+
+}
